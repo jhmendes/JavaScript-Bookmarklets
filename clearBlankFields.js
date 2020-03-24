da920cfbeacab819f0de9510f4a331d8a55733a0
John Mendes <johnhenrymendes@gmail.com>
2020-03-23 20:34:31 -0400

    cleaned up script for outlook printing

--- clearBlankFields.js
+++ clearBlankFields.js
@@ -1,15 +1,15 @@
-// target the email div in print screen
+// Target the inner print panel and grab all the paragraphs
 
 var printPanel = document.querySelector('.ms-Panel-content');
 var innerPrintPanel = printPanel.querySelector('.x_no-mail-styles');
 var textBlocks = innerPrintPanel.querySelectorAll('p');
 
-// Create Array from  node list
+// Create Array from  node list of paragraphs
 var btnsArr = Array.prototype.slice.call(textBlocks);
 
 
-//Loop through each element
-// El here is a paragraph
+//Loop through each element and check to see if the span has content.  If not, remove the parent element 
+
 
 btnsArr.forEach( function(el) {
   if(el.lastElementChild.innerText == '') {
